3639c505e38fe5982a6931a3f7fb4725e48eb5d8
Enzo <enzosousa1@yahoo.com>
2025-08-05 01:07:25 +0200

    fixé

--- components/Home.tsx
+++ components/Home.tsx
@@ -9,7 +9,6 @@ const Home = () => {
       className="flex flex-col xl:flex-row items-center xl:justify-between max-xs:mt-22 mt-32 xl:mt-60"
       id="home"
     >
-      <h1 className="hidden">Enzo Sousa Portfolio</h1>
       <div className="text-center xl:text-left order-2 xl:order-1 mt-4 xl:mt-0">
         <span className="text-xl">Web developer</span>
         <h1 className="h1 mb-2">
